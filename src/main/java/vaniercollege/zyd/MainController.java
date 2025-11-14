7d7a87b4b0ab8db57cbc12cf9200ab7ceae8bc0c
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-11-14 15:19:35 -0500

    feat: Add Handles to Items

--- src/main/java/vaniercollege/zyd/MainController.java
+++ src/main/java/vaniercollege/zyd/MainController.java
@@ -1,14 +1,102 @@
 package vaniercollege.zyd;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem closeItem;
+
+    @FXML
+    private MenuItem copyItem;
+
+    @FXML
+    private MenuItem findItem;
+
+    @FXML
+    private CheckMenuItem gridItem;
+
+    @FXML
+    private MenuItem largeItem;
+
+    @FXML
+    private MenuItem mediumItem;
+
+    @FXML
+    private MenuItem newItem;
+
+    @FXML
+    private MenuItem openItem;
+
+    @FXML
+    private MenuItem replaceItem;
+
+    @FXML
+    private MenuItem saveItem;
+
+    @FXML
+    private Menu searchMenu;
+
+    @FXML
+    private MenuItem smallItem;
+
+    @FXML
+    void handleClose(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleCopy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFind(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleGrid(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleOpen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReplace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSizeLarge(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSizeMedium(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSizeSmall(ActionEvent event) {
+
+    }
+
 }
