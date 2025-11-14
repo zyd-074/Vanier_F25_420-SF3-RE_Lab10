1258ae029539c3f39bc6d969b5834641f7d43d6a
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-11-14 16:06:41 -0500

    feat: Add Handlers to All Items

--- src/main/java/vaniercollege/zyd/MainController.java
+++ src/main/java/vaniercollege/zyd/MainController.java
@@ -2,11 +2,16 @@ package vaniercollege.zyd;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.FileChooser;
+import javafx.stage.Popup;
+
+import java.io.File;
 
 public class MainController {
+    @FXML
+    public ToggleGroup sizeGroup;
 
     @FXML
     private MenuItem closeItem;
@@ -21,10 +26,10 @@ public class MainController {
     private CheckMenuItem gridItem;
 
     @FXML
-    private MenuItem largeItem;
+    private RadioMenuItem largeItem;
 
     @FXML
-    private MenuItem mediumItem;
+    private RadioMenuItem mediumItem;
 
     @FXML
     private MenuItem newItem;
@@ -42,61 +47,103 @@ public class MainController {
     private Menu searchMenu;
 
     @FXML
-    private MenuItem smallItem;
+    private RadioMenuItem smallItem;
 
     @FXML
     void handleClose(ActionEvent event) {
-
+        newItem.setDisable(false);
+        openItem.setDisable(false);
+        closeItem.setDisable(true);
+        saveItem.setDisable(true);
+        System.out.println("User closed the files");
     }
 
     @FXML
     void handleCopy(ActionEvent event) {
-
+        System.out.println("COPY has been pressed");
     }
 
     @FXML
     void handleFind(ActionEvent event) {
-
+        System.out.println("Find has been pressed");
     }
 
     @FXML
     void handleGrid(ActionEvent event) {
-
+        if (gridItem.isSelected()) {
+            System.out.println("USE GRIDLINES selected");
+        } else {
+            System.out.println("USE GRIDLINES unselected");
+        }
     }
 
     @FXML
     void handleNew(ActionEvent event) {
-
+        System.out.println("NEW has been pressed");
+        newItem.setDisable(true);
+        openItem.setDisable(true);
+        closeItem.setDisable(false);
+        saveItem.setDisable(false);
     }
 
     @FXML
     void handleOpen(ActionEvent event) {
-
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open File");
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Files", "*")
+        );
+        File selected = fc.showOpenDialog(new Popup());
+        if (selected != null) {
+            String path = selected.getPath();
+            System.out.println("User opened: " + path);
+        }
+        closeItem.setDisable(false);
+        saveItem.setDisable(false);
+        openItem.setDisable(true);
+        newItem.setDisable(true);
     }
 
     @FXML
     void handleReplace(ActionEvent event) {
-
+        System.out.println("REPLACE has been pressed");
     }
 
     @FXML
     void handleSave(ActionEvent event) {
-
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Save File");
+        File selected = fc.showSaveDialog(new Popup());
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Files", "*")
+        );
+        if (selected != null) {
+            String path = selected.getPath();
+            System.out.println("User saved file to: " + path);
+        }
     }
 
     @FXML
     void handleSizeLarge(ActionEvent event) {
-
+        System.out.println("User switched to LARGE");
     }
 
     @FXML
     void handleSizeMedium(ActionEvent event) {
-
+        System.out.println("User switched to MEDIUM");
     }
 
     @FXML
     void handleSizeSmall(ActionEvent event) {
-
+        System.out.println("User switched to SMALL");
     }
 
+    public void initialize() {
+        closeItem.setDisable(true);
+        saveItem.setDisable(true);
+        closeItem.setAccelerator(KeyCombination.keyCombination("Ctrl+C"));
+        newItem.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+        openItem.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+        saveItem.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+    }
 }
